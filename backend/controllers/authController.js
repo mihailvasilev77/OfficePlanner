edaef7b290391827bec1fcdea9caba8bdfd75664
mishopazarlucka <mihailbg2005@gmail.com>
2024-02-11 15:24:07 +0200

    Added login front and back end

---
+++ backend/controllers/authController.js
@@ -0,0 +1,45 @@
+const User = require('../model/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const day = 24 * 60 * 60 * 1000;    
+
+const handleLogin = async (req, res) => {
+    const { user, pwd } = req.body;
+    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
+
+    const foundUser = await User.findOne({ username: user }).exec();
+    if (!foundUser) return res.sendStatus(401);
+    const match = await bcrypt.compare(pwd, foundUser.password);
+    if (match) {
+        const roles = Object.values(foundUser.roles).filter(Boolean);
+        const accessToken = jwt.sign(
+            {
+                "UserInfo": {
+                    "username": foundUser.username,
+                    "roles": roles
+                }
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '5m' }
+        );
+        const refreshToken = jwt.sign(
+            { "username": foundUser.username },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1d' }
+        );
+        foundUser.refreshToken = refreshToken;
+        const result = await foundUser.save();
+        console.log(result);
+        console.log(roles);
+
+        res.cookie('jwt', refreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: day });
+
+        res.json({ roles, accessToken });
+
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+module.exports = { handleLogin };
